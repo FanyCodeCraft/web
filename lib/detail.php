6adc8d7275179de3bad04c68ce21c80c087e41e9
FanyCodeCraft <tio34981@gmail.com>
2024-12-28 17:42:11 +0700

    menambahkan navbar ke detail.php dan edit.php

--- lib/detail.php
+++ lib/detail.php
@@ -1,3 +1,38 @@
+<!-- Navbar -->
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="#">Invoice</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item active">
+                    <a class="nav-link" href="../index.php">Beranda</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="../main/destinasi.php">Destinasi</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="../main/tentang.php">Tentang Kami</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="../main/kontak.php">Kontak</a>
+                </li>
+                <li  class="nav-item">
+                    <a class="nav-link" href="../main/pesanan.php">Pemesanan</a>
+                </li>
+                <li  class="nav-item">
+                    <a class="nav-link" href="../lib/daftar.php">Daftar Pemesanan</a>
+                </li>
+            </ul>
+             <!-- Form Pencarian -->
+             <form class="form-inline ml-auto">
+                <input class="form-control mr-2" type="search" placeholder="Cari..." aria-label="Search">
+                <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Cari</button>
+            </form>
+        </div>
+    </nav>
+
 <?php
 include 'koneksi.php';
 
@@ -39,7 +74,7 @@ if (mysqli_num_rows($query) == 0) {
   <div class="container">
     <div class="card mt-2">
       <div class="card-header bg-dark text-white">
-        Detail Pemesanan Paket Wisata #<?=$detail[0]?>
+        Detail Pemesanan Paket Wisata <?=$detail[0]?>
       </div>
       <div class="card-body">
         <div class="mb-3">
